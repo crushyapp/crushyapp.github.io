af7be43570d4db402fbaa1fe736fc5dd42dc1125
abandohess <bandersman@gmail.com>
2019-07-18 23:48:38 -0400

    adfa

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const store = createStore(reducers, {}, compose(
   window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f,
 ));
 
-// comment/
+// comment
 // we now wrap App in a Provider
 
 
